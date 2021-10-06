c71580bb08e922ff148fdc15e4090bde4ebf4ff9
Fabio Bocci <74119740+FabioBocci@users.noreply.github.com>
2021-10-06 21:42:53 +0200

    refresh commands and removed some todos (#5)
    * new command for refresh
    
    * put log and removed todo
    
    * setting command permissions
    
    * removed useless command
    
    Co-authored-by: FabioBocci <noobgamingita@gmail.com>

--- src/main/java/com/playmonumenta/networkchat/ChannelParty.java
+++ src/main/java/com/playmonumenta/networkchat/ChannelParty.java
@@ -106,7 +106,7 @@ public class ChannelParty extends Channel implements ChannelInviteOnly {
 					playerId = UUID.fromString(playerPermEntry.getKey());
 					playerPermsJson = playerPermEntry.getValue().getAsJsonObject();
 				} catch (Exception e) {
-					// TODO Log this
+					NetworkChatPlugin.getInstance().getLogger().warning("Catch exeption during converting json to channel Party reason: " + e.getMessage());
 					continue;
 				}
 				ChannelPerms playerPerms = ChannelPerms.fromJson(playerPermsJson);
