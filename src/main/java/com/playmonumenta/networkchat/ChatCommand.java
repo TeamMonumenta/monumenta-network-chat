cbbf1d451b49db62dd20fcce81e03f081e967893
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-10-20 09:23:15 -0700

    add listplayers permission (#46)

--- src/main/java/com/playmonumenta/networkchat/ChatCommand.java
+++ src/main/java/com/playmonumenta/networkchat/ChatCommand.java
@@ -715,6 +715,7 @@ public class ChatCommand {
 			arguments.add(new MultiLiteralArgument("listplayers"));
 			new CommandAPICommand(baseCommand)
 				.withArguments(arguments)
+				.withPermission("networkchat.listplayers")
 				.executesNative((sender, args) -> {
 					RemotePlayerManager.showOnlinePlayers(CommandUtils.getCallee(sender));
 					return 1;
