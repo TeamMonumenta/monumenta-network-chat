ab12f38f9e42e68cb35b2dfe74a57132444cab1b
Timothy Southwick <NickNackGus@gmail.com>
2023-01-11 16:59:06 -0500

    Updated Command API to 8.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("net.kyori:adventure-text-minimessage:4.11.0")
     implementation("org.apache.commons:commons-text:1.3")
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
-    implementation("dev.jorel.CommandAPI:commandapi-shade:8.7.0")
+    implementation("dev.jorel.CommandAPI:commandapi-shade:8.7.1")
     compileOnly("com.playmonumenta:monumenta-network-relay:1.1")
     compileOnly("com.playmonumenta:redissync:4.1")
     compileOnly("io.lettuce:lettuce-core:5.3.5.RELEASE")
