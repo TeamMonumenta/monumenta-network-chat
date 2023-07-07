74941ca78b22c03a638c7290c0f27860e00e228a
Timothy Southwick <NickNackGus@gmail.com>
2023-07-07 01:15:31 -0400

    Updated play-deploy to reflect host changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -262,9 +262,11 @@ tasks.create("play-deploy") {
                 put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m8/server_config/plugins")
                 put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m11/server_config/plugins")
 				put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m13/server_config/plugins")
+				put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m15/server_config/plugins")
                 execute("cd /home/epic/play/m8/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
                 execute("cd /home/epic/play/m11/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
 				execute("cd /home/epic/play/m13/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
+				execute("cd /home/epic/play/m15/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
             }
         }
     }
