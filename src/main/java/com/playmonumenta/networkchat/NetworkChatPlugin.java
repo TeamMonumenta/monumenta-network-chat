9d68d7d034b69ec558dc568418d9fb339dc853ae
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2023-11-03 18:39:18 -0700

    init commit

--- src/main/java/com/playmonumenta/networkchat/NetworkChatPlugin.java
+++ src/main/java/com/playmonumenta/networkchat/NetworkChatPlugin.java
@@ -157,6 +157,17 @@ public class NetworkChatPlugin extends JavaPlugin implements Listener {
 			MessagingUtils.sendStackTrace(Bukkit.getConsoleSender(), e);
 		}
 
+		try {
+			mGlobalChatFilter.addFilter(Bukkit.getConsoleSender(),
+					"Roll",
+					false,
+					"(?<=^|[^\\\\])\\|\\|([^|]*[^|\\s\\\\][^|\\\\]*)\\|\\|",
+					false)
+				.replacementMessage("<b><hover:show_text:\"$\\1\">SPOILER</hover></b>");
+		} catch (WrapperCommandSyntaxException e) {
+			MessagingUtils.sendStackTrace(Bukkit.getConsoleSender(), e);
+		}
+
 		ChangeLogLevel.register();
 		@Nullable ZipFile zip = null;
 		try {
