6d716195f4a64d9d68007f6e40bad83541894355
dmdinnc <38630939+dmdinnc@users.noreply.github.com>
2023-11-10 18:05:39 -0500

    add max number roller, adjust formatting to use space in roll

--- src/main/java/com/playmonumenta/networkchat/inlinereplacements/DiceRollReplacement.java
+++ src/main/java/com/playmonumenta/networkchat/inlinereplacements/DiceRollReplacement.java
@@ -2,6 +2,7 @@ package com.playmonumenta.networkchat.inlinereplacements;
 
 import com.playmonumenta.networkchat.utils.MMLog;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.command.CommandSender;
 import org.intellij.lang.annotations.RegExp;
@@ -9,11 +10,13 @@ import org.intellij.lang.annotations.RegExp;
 public class DiceRollReplacement extends InlineReplacement {
 
 	@RegExp
-	private static final String mDiceDataGivenRegex = ".*:[1-9]?[0-9]*d[1-9][0-9]*.*";
+	private static final String mDiceDataGivenRegex = ".* [1-9]?[0-9]*d[1-9][0-9]*.*";
+	@RegExp
+	private static final String mMaxDataGivenRegex = ".* [1-9][0-9]*.*";
 
 	public DiceRollReplacement() {
 		super("Dice Roll",
-			"<roll(:[1-9]?[0-9]*d[1-9][0-9]*)?>",
+			"<roll(( [1-9]?[0-9]*d[1-9][0-9]*)|( [1-9][0-9]*))?>",
 			"roll");
 	}
 
@@ -26,10 +29,23 @@ public class DiceRollReplacement extends InlineReplacement {
 				MMLog.info("DRR: matched string '" + matchResult.content() + "' with regex:" + mRegex);
 				String regexResult = matchResult.content();
 				Pair<Integer, Integer> diceData = Pair.of(1, 100);
-				if (regexResult.matches(mDiceDataGivenRegex)) {
+				boolean isDiceFormat = true;
+				if (regexResult.matches(mMaxDataGivenRegex)) {
+					MMLog.info("DRR: matched max number regex with:" + regexResult);
+					try {
+						String maxInfo = regexResult.replaceAll("(<|>)", "")
+							.split(" ")[1];
+						MMLog.info("DRR: maxInfo:" + maxInfo);
+						diceData = Pair.of(1, Integer.parseInt(maxInfo));
+						isDiceFormat = false;
+					} catch (Exception e) {
+						return input;
+					}
+				} else if (regexResult.matches(mDiceDataGivenRegex)) {
+					MMLog.info("DRR: matched dice regex with:" + matchResult.content());
 					try {
 						String[] getDiceInfo = regexResult.replaceAll("(<|>)", "")
-							.split(":")[1].split("d");
+							.split(" ")[1].split("d");
 						if (!getDiceInfo[0].equals("")) {
 							diceData = Pair.of(Integer.parseInt(getDiceInfo[0]),
 								Integer.parseInt(getDiceInfo[1]));
@@ -41,11 +57,17 @@ public class DiceRollReplacement extends InlineReplacement {
 					}
 				}
 				int rollValue = 0;
-				for (int i = 0; i < diceData.getLeft(); i++) {
-					rollValue += (int) (Math.random() * diceData.getRight()) + 1;
+				if (isDiceFormat) {
+					for (int i = 0; i < diceData.getLeft(); i++) {
+						rollValue += (int) (Math.random() * diceData.getRight()) + 1;
+					}
+					return Component.text("ROLL").decoration(TextDecoration.BOLD, true).hoverEvent(Component.text(rollValue + " out of " + diceData.getLeft()
+						+ "d" + diceData.getRight() + " (" + diceData.getLeft() * diceData.getRight() + ")"));
+				} else {
+					rollValue = (int) (Math.random() * diceData.getRight()) + 1;
+					return Component.text("ROLL").decoration(TextDecoration.BOLD, true)
+						.hoverEvent(Component.text(rollValue + " out of " + diceData.getRight()));
 				}
-				return Component.text(rollValue + " out of " + diceData.getLeft()
-					+ "d" + diceData.getRight() + " (" + diceData.getLeft() * diceData.getRight() + ")");
 			}));
 		return output;
 	}
