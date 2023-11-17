168ff470294dd5433f821d8d52e7be8e41087169
Timothy Southwick <NickNackGus@gmail.com>
2023-11-17 01:43:39 -0500

    Blacklist signs from item hover replacement

--- src/main/java/com/playmonumenta/networkchat/inlinereplacements/ItemHoverReplacement.java
+++ src/main/java/com/playmonumenta/networkchat/inlinereplacements/ItemHoverReplacement.java
@@ -1,7 +1,8 @@
 package com.playmonumenta.networkchat.inlinereplacements;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import net.kyori.adventure.text.Component;
 import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.Material;
@@ -11,17 +12,25 @@ import org.intellij.lang.annotations.RegExp;
 
 public class ItemHoverReplacement extends InlineReplacement {
 
-	private static final List<Material> BANNED_MATERIALS = new ArrayList<>(
-		List.of(Material.SHULKER_BOX, Material.BLACK_SHULKER_BOX,
-			Material.BLUE_SHULKER_BOX, Material.BROWN_SHULKER_BOX,
-			Material.CYAN_SHULKER_BOX, Material.GRAY_SHULKER_BOX,
-			Material.GREEN_SHULKER_BOX, Material.LIGHT_BLUE_SHULKER_BOX,
-			Material.LIGHT_GRAY_SHULKER_BOX, Material.LIME_SHULKER_BOX,
-			Material.MAGENTA_SHULKER_BOX, Material.ORANGE_SHULKER_BOX,
-			Material.PINK_SHULKER_BOX, Material.PURPLE_SHULKER_BOX,
-			Material.RED_SHULKER_BOX, Material.WHITE_SHULKER_BOX,
-			Material.YELLOW_SHULKER_BOX, Material.WRITTEN_BOOK)
-	);
+	private static final Set<Material> BANNED_MATERIALS;
+
+	static {
+		Set<Material> bannedMaterials = new HashSet<>();
+		bannedMaterials.add(Material.WRITTEN_BOOK);
+
+		for (Material mat : Material.values()) {
+			if (!mat.isItem()) {
+				continue;
+			}
+
+			String id = mat.getKey().toString();
+			if (id.endsWith("shulker_box") || id.endsWith("_sign")) {
+				bannedMaterials.add(mat);
+			}
+		}
+
+		BANNED_MATERIALS = bannedMaterials.stream().collect(Collectors.toUnmodifiableSet());
+	}
 
 	@RegExp
 	private static final String mMainhandRegex = "<mainhand>";
