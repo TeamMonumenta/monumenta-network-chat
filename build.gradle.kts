f18f9f4313aa7fbce07f08512c7e114a80ec6700
Timothy Southwick <NickNackGus@gmail.com>
2024-03-02 14:24:58 -0500

    Updated play-deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -273,14 +273,12 @@ tasks.create("play-deploy") {
     doLast {
         ssh.runSessions {
             session(adminssh) {
-                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m8/server_config/plugins")
-                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m11/server_config/plugins")
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m12/server_config/plugins")
 				put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m13/server_config/plugins")
-				put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m15/server_config/plugins")
-                execute("cd /home/epic/play/m8/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
-                execute("cd /home/epic/play/m11/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
+				put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m17/server_config/plugins")
+				execute("cd /home/epic/play/m12/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
 				execute("cd /home/epic/play/m13/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
-				execute("cd /home/epic/play/m15/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
+				execute("cd /home/epic/play/m17/server_config/plugins && rm -f NetworkChat.jar && ln -s " + shadowJar.archiveFileName.get() + " NetworkChat.jar")
             }
         }
     }
