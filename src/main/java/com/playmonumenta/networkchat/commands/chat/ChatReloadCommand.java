f2ce2c43c18e3f28e7f42735b9e3ffb9ca4cbe7e
Timothy Southwick <NickNackGus@gmail.com>
2024-05-16 23:12:20 -0400

    Added missing reload permission

--- src/main/java/com/playmonumenta/networkchat/commands/chat/ChatReloadCommand.java
+++ src/main/java/com/playmonumenta/networkchat/commands/chat/ChatReloadCommand.java
@@ -2,6 +2,7 @@ package com.playmonumenta.networkchat.commands.chat;
 
 import com.playmonumenta.networkchat.NetworkChatPlugin;
 import com.playmonumenta.networkchat.commands.ChatCommand;
+import com.playmonumenta.networkchat.utils.CommandUtils;
 import dev.jorel.commandapi.arguments.LiteralArgument;
 
 public class ChatReloadCommand {
@@ -9,6 +10,10 @@ public class ChatReloadCommand {
 		ChatCommand.getBaseCommand()
 			.withArguments(new LiteralArgument("reload"))
 			.executesNative((sender, args) -> {
+				if (!CommandUtils.hasPermission(sender, "networkchat.reload")) {
+					throw CommandUtils.fail(sender, "You do not have permission to reload NetworkChat.");
+				}
+
 				NetworkChatPlugin.reload(sender);
 			})
 			.register();
