488cd43268b749d12f56050a2e7a5b218f39cf2e
Timothy Southwick <NickNackGus@gmail.com>
2024-05-28 20:51:27 -0400

    Updated monumenta-deploy dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1" // Generates plugin.yml
     id("net.ltgt.errorprone") version "2.0.2"
     id("net.ltgt.nullaway") version "1.3.0"
-	id("com.playmonumenta.deployment") version "1.0"
+	id("com.playmonumenta.deployment") version "1.2"
     checkstyle
     pmd
 }
