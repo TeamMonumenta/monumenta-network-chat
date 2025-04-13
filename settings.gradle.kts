8e30cab0f965cad06296d9de34fa5a6a62633c71
Floweynt <guywithanaxe42@gmail.com>
2025-04-13 13:47:12 -0500

    try and fix deployment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,7 @@ pluginManagement {
 	repositories {
 		gradlePluginPortal()
 		maven("https://maven.playmonumenta.com/releases/")
+		// TODO: don't merge unless this is removed
+		maven("https://maven.playmonumenta.com/snapshots/")
 	}
 }
